b9f3429c6e836399abdb4d0611fdff57f1c1cfa0
Daniil Pankrashin <daniil@pankrashin.com>
2025-06-19 19:55:59 +0200

    Changes to add support for chrome extension manifest v3

--- js/background.js
+++ js/background.js
@@ -1,91 +1,140 @@
 /*!
-* CSSViewer, A Google Chrome Extension for fellow web developers, web designers, and hobbyists.
-*
-* https://github.com/miled/cssviewer
-* https://chrome.google.com/webstore/detail/cssviewer/ggfgijbpiheegefliciemofobhmofgce
-*
-* This source code is licensed under the GNU General Public License,
-* Version 2. See the file COPYING for more details.
-*/
+ * CSSViewer, A Google Chrome Extension for fellow web developers, web designers, and hobbyists.
+ *
+ * https://github.com/miled/cssviewer
+ * https://chrome.google.com/webstore/detail/cssviewer/ggfgijbpiheegefliciemofobhmofgce
+ *
+ * This source code is licensed under the GNU General Public License,
+ * Version 2. See the file COPYING for more details.
+ */
 
-var cssViewerLoaded              = false; 
-var cssCiewerContextMenusParent  = null;
+var cssViewerLoaded = false;
+var cssCiewerContextMenusParent = null;
 
 // Check whether new version is installed
-chrome.runtime.onInstalled.addListener(function(details){
-	if(details.reason == "install" || details.reason == "update" ){
-		chrome.tabs.create( {url: "option.html"} );
-	}
+chrome.runtime.onInstalled.addListener(function (details) {
+        if (details.reason == "install" || details.reason == "update") {
+                chrome.tabs.create({ url: "option.html" });
+        }
 });
 
 /*
-* Inject cssviewer.js/cssviewer.css into the current page
-*/
-chrome.browserAction.onClicked.addListener(function(tab)
-{
-	if( tab.url.indexOf("https://chrome.google.com") == 0 || tab.url.indexOf("chrome://") == 0 )
-	{
-		alert( "CSSViewer doesn't work on Google Chrome webstore!" );
+ * Inject cssviewer.js/cssviewer.css into the current page
+ */
+chrome.action.onClicked.addListener(async function (tab) {
+        if (
+                tab.url.startsWith("https://chrome.google.com") ||
+                tab.url.startsWith("chrome://")
+        ) {
+                chrome.notifications.create({
+                        type: "basic",
+                        iconUrl: "img/16.png",
+                        title: "CSSViewer",
+                        message: "CSSViewer doesn't work on Google Chrome webstore!",
+                });
+                return;
+        }
 
-		return;
-	}
+        if (!cssViewerLoaded) {
+                cssCiewerContextMenusParent = await chrome.contextMenus.create({
+                        title: "CSSViewer console",
+                        contexts: ["all"],
+                });
 
-	if( ! cssViewerLoaded ) 
-	{
-		cssCiewerContextMenusParent  = chrome.contextMenus.create( { "title" : "CSSViewer console", contexts:["all"] } );
+                await chrome.contextMenus.create({
+                        title: "element",
+                        contexts: ["all"],
+                        parentId: cssCiewerContextMenusParent,
+                        id: "cssviewer-element",
+                });
+                await chrome.contextMenus.create({
+                        title: "element.id",
+                        contexts: ["all"],
+                        parentId: cssCiewerContextMenusParent,
+                        id: "cssviewer-element-id",
+                });
+                await chrome.contextMenus.create({
+                        title: "element.tagName",
+                        contexts: ["all"],
+                        parentId: cssCiewerContextMenusParent,
+                        id: "cssviewer-element-tagName",
+                });
+                await chrome.contextMenus.create({
+                        title: "element.className",
+                        contexts: ["all"],
+                        parentId: cssCiewerContextMenusParent,
+                        id: "cssviewer-element-className",
+                });
+                await chrome.contextMenus.create({
+                        title: "element.style",
+                        contexts: ["all"],
+                        parentId: cssCiewerContextMenusParent,
+                        id: "cssviewer-element-style",
+                });
+                await chrome.contextMenus.create({
+                        title: "element.cssText",
+                        contexts: ["all"],
+                        parentId: cssCiewerContextMenusParent,
+                        id: "cssviewer-element-cssText",
+                });
+                await chrome.contextMenus.create({
+                        title: "element.getComputedStyle",
+                        contexts: ["all"],
+                        parentId: cssCiewerContextMenusParent,
+                        id: "cssviewer-element-getComputedStyle",
+                });
+                await chrome.contextMenus.create({
+                        title: "element.simpleCssDefinition",
+                        contexts: ["all"],
+                        parentId: cssCiewerContextMenusParent,
+                        id: "cssviewer-element-simpleCssDefinition",
+                });
+                cssViewerLoaded = true;
+        }
 
-		chrome.contextMenus.create( { "title": "element"                    , contexts:["all"] , "parentId": cssCiewerContextMenusParent, "onclick": cssCiewerDebugEl } );
-		chrome.contextMenus.create( { "title": "element.id"                 , contexts:["all"] , "parentId": cssCiewerContextMenusParent, "onclick": cssCiewerDebugElId } );
-		chrome.contextMenus.create( { "title": "element.tagName"            , contexts:["all"] , "parentId": cssCiewerContextMenusParent, "onclick": cssCiewerDebugElTagName } );
-		chrome.contextMenus.create( { "title": "element.className"          , contexts:["all"] , "parentId": cssCiewerContextMenusParent, "onclick": cssCiewerDebugElClassName } );
-		chrome.contextMenus.create( { "title": "element.style"              , contexts:["all"] , "parentId": cssCiewerContextMenusParent, "onclick": cssCiewerDebugElStyle } );
-		chrome.contextMenus.create( { "title": "element.cssText"            , contexts:["all"] , "parentId": cssCiewerContextMenusParent, "onclick": cssCiewerDebugElCssText } );
-		chrome.contextMenus.create( { "title": "element.getComputedStyle"   , contexts:["all"] , "parentId": cssCiewerContextMenusParent, "onclick": cssCiewerDebugElGetComputedStyle } );
-		chrome.contextMenus.create( { "title": "element.simpleCssDefinition", contexts:["all"] , "parentId": cssCiewerContextMenusParent, "onclick": cssCiewerDebugElSimpleCssDefinition } );
-	}
-
-	chrome.tabs.executeScript(tab.id, {file:'js/cssviewer.js'});
-	chrome.tabs.insertCSS(tab.id, {file:'css/cssviewer.css'});
-
-	cssViewerLoaded = true;
+        await chrome.scripting.insertCSS({
+                target: { tabId: tab.id },
+                files: ["css/cssviewer.css"],
+        });
+        await chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: ["js/cssviewer.js"],
+        });
 });
 
-function cssCiewerDebugEl( info, tab )
-{
-	chrome.tabs.executeScript(tab.id, {code:'cssViewerCopyCssToConsole("el")'});
-}
-
-function cssCiewerDebugElId( info, tab )
-{
-	chrome.tabs.executeScript(tab.id, {code:'cssViewerCopyCssToConsole("id")'});
-}
-
-function cssCiewerDebugElTagName( info, tab )
-{
-	chrome.tabs.executeScript(tab.id, {code:'cssViewerCopyCssToConsole("tagName")'});
-}
-
-function cssCiewerDebugElClassName( info, tab )
-{
-	chrome.tabs.executeScript(tab.id, {code:'cssViewerCopyCssToConsole("className")'});
-}
-
-function cssCiewerDebugElStyle( info, tab )
-{
-	chrome.tabs.executeScript(tab.id, {code:'cssViewerCopyCssToConsole("style")'});
-}
-
-function cssCiewerDebugElCssText( info, tab )
-{
-	chrome.tabs.executeScript(tab.id, {code:'cssViewerCopyCssToConsole("cssText")'});
-}
- 
-function cssCiewerDebugElGetComputedStyle( info, tab )
-{
-	chrome.tabs.executeScript(tab.id, {code:'cssViewerCopyCssToConsole("getComputedStyle")'});
-}
-
-function cssCiewerDebugElSimpleCssDefinition( info, tab )
-{
-	chrome.tabs.executeScript(tab.id, {code:'cssViewerCopyCssToConsole("simpleCssDefinition")'});
-}
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+        let codeType = null;
+        switch (info.menuItemId) {
+                case "cssviewer-element":
+                        codeType = "el";
+                        break;
+                case "cssviewer-element-id":
+                        codeType = "id";
+                        break;
+                case "cssviewer-element-tagName":
+                        codeType = "tagName";
+                        break;
+                case "cssviewer-element-className":
+                        codeType = "className";
+                        break;
+                case "cssviewer-element-style":
+                        codeType = "style";
+                        break;
+                case "cssviewer-element-cssText":
+                        codeType = "cssText";
+                        break;
+                case "cssviewer-element-getComputedStyle":
+                        codeType = "getComputedStyle";
+                        break;
+                case "cssviewer-element-simpleCssDefinition":
+                        codeType = "simpleCssDefinition";
+                        break;
+        }
+        if (codeType) {
+                chrome.scripting.executeScript({
+                        target: { tabId: tab.id },
+                        func: (type) => window.cssViewerCopyCssToConsole(type),
+                        args: [codeType],
+                });
+        }
+});
